0d6fb293fc49fcc589ad49b62eb3ce45189e352c
Matthew Slipper <me@matthewslipper.com>
2024-08-30 11:39:47 -0600

    --wip-- [skip ci]

--- packages/contracts-bedrock/deploy.sh
+++ packages/contracts-bedrock/deploy.sh
@@ -33,7 +33,6 @@ echoerr "> Using impl salt: $DEPLOY_IMPL_SALT"
 
 echoerr "> Checking CREATE2 deployer..."
 
-set +e
 codesize=$(cast codesize 0x4e59b44847b379578588920cA78FbF26c0B4956C)
 if [ "$codesize" == "0" ]; then
   cast publish 0xf8a58085174876e800830186a08080b853604580600e600039806000f350fe7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe03601600081602082378035828234f58015156039578182fd5b8082525050506014600cf31ba02222222222222222222222222222222222222222222222222222222222222222a02222222222222222222222222222222222222222222222222222222222222222
@@ -53,13 +52,11 @@ else
   echoerr "> CREATE2 deployer failed with unexpected exit code $?."
   exit 1
 fi
-set -e
 
 verify_flag=""
 if [ -n "${DEPLOY_VERIFY:-}" ]; then
   verify_flag="--verify"
 fi
-
 
 echoerr "> Extracting deploy config..."
 cat "$DEPLOY_STATE_PATH" | jq -r '.deployConfig' > ./deploy-config/deploy-config.json
